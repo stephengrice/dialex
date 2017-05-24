b08690504b6318e83c8aadddda0f9249fbecdc98
Stephen Grice <stephengrice@live.com>
2017-05-24 14:59:07 -0400

    Add exercises tests

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,13 +1,11 @@
 class ExercisesController < ApplicationController
   def grade
     @exercise = Exercise.find(params[:id])
-    @input = params[:input]
-    if @input.nil?
+    @translation = params[:translation]
+    if @translation.nil?
       render json: {correct: false}
     else
-      # Return true if the answer matches. mb_chars.downcase.to_s allows for cyrillic case insensitivity
-      @result = {correct: @exercise.translation.mb_chars.downcase.to_s == @input.mb_chars.downcase.to_s}
-      #@result = {correct: @exercise.translation.downcase == @input.downcase}
+      @result = {correct: @exercise.translation.mb_chars.downcase.to_s == @translation.mb_chars.downcase.to_s}
       render json: @result.to_json
     end
   end
--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -4,4 +4,33 @@ class ExercisesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should mark empty request as wrong" do
+    get grade_path(exercises(:one).id)
+    assert_response :success
+    assert_equal false, JSON.parse(@response.body)["correct"]
+  end
+  
+  test "should mark incorrect translation as wrong" do
+    get grade_path(exercises(:one).id), params: { translation: "bogus" }
+    assert_response :success
+    assert_equal false, JSON.parse(@response.body)["correct"]
+  end
+  
+  test "should mark correct translation as correct" do
+    get grade_path(exercises(:one).id), params: { translation: exercises(:one).translation } 
+    assert_response :success
+    assert_equal true, JSON.parse(@response.body)["correct"]
+  end
+  
+  test "should be case insensitive for latin alphabet" do
+    get grade_path(exercises(:one).id), params: { translation: exercises(:one).translation.upcase }
+    assert_response :success
+    assert_equal true, JSON.parse(@response.body)["correct"]
+  end
+  
+  test "should be case insensitive for cyrillic alphabet" do
+    get grade_path(exercises(:ru).id), params: { translation: exercises(:ru).translation.mb_chars.upcase.to_s }
+    assert_response :success
+    assert_equal true, JSON.parse(@response.body)["correct"]
+  end
 end
