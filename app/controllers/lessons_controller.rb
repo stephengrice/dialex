2e97ed5bcf03d842f89a1af31195697404ae382e
Stephen Grice <stephengrice@live.com>
2017-05-26 11:19:58 -0400

    Improve user interface

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,7 @@
 class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
+    @language = Language.find(@lesson.language_id)
     @exercises = Exercise.where(lesson: @lesson)
   end
 end
