852199b0d3dc6a74adba86416f280e417159ea87
Stephen Grice <stephengrice@live.com>
2018-11-02 16:53:24 -0400

    Commit old changes

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,4 +1,8 @@
 class Credit < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+  
+  def has_current_user
+    
+  end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,4 @@
 class Lesson < ApplicationRecord
   belongs_to :language
+  has_many :credits
 end
